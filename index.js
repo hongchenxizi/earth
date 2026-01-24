ab2270f793ac65d500335a56d4604903125056d3
tydhome <tydhome@gmail.com>
2026-01-24 10:09:40 +0800

    ok

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ const crypto = require('crypto');
 const { Buffer } = require('buffer');
 const { exec, execSync } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
-const UUID = process.env.UUID || '5efabea4-f6d4-91fd-b8f0-17e004c89c60'; // 运行哪吒v1,在不同的平台需要改UUID,否则会被覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
-const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口                
-const DOMAIN = process.env.DOMAIN || 'your-domain.com';    // 填写项目域名或已反代的域名，不带前缀，建议填已反代的域名
-const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
-const WSPATH = process.env.WSPATH || UUID.slice(0, 8);     // 节点路径，默认获取uuid前8位
-const SUB_PATH = process.env.SUB_PATH || 'sub';            // 获取节点的订阅路径
-const NAME = process.env.NAME || '';                       // 节点名称
-const PORT = process.env.PORT || 3000;                     // http和ws服务端口
+const UUID = process.env.UUID || '9c60cbd2-782b-1620-9ca9-829f1c2cdd38';
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       
+const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 443,8443,2096,2087,2083,2053
+const NEZHA_KEY = process.env.NEZHA_KEY || '';                  
+const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
+const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      
+const WSPATH = process.env.WSPATH || UUID.slice(0, 8);     
+const SUB_PATH = process.env.SUB_PATH || 'you';            
+const NAME = process.env.NAME || '';                       
+const PORT = process.env.PORT || 3000;                     
 
 let uuid = UUID.replace(/-/g, ""), CurrentDomain = DOMAIN, Tls = 'tls', CurrentPort = 443, ISP = '';
 const DNS_SERVERS = ['8.8.4.4', '1.1.1.1'];
@@ -463,27 +463,6 @@ uuid: ${UUID}`;
   }
 };
 
-async function addAccessTask() {
-  if (!AUTO_ACCESS) return;
-
-  if (!DOMAIN) {
-    return;
-  }
-  const fullURL = `https://${DOMAIN}/${SUB_PATH}`;
-  try {
-    const res = await axios.post("https://oooo.serv00.net/add-url", {
-      url: fullURL
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log('Automatic Access Task added successfully');
-  } catch (error) {
-    // console.error('Error adding Task:', error.message);
-  }
-}
-
 const delFiles = () => {
   ['npm', 'config.yaml'].forEach(file => fs.unlink(file, () => { }));
 };
@@ -493,6 +472,5 @@ httpServer.listen(PORT, () => {
   setTimeout(() => {
     delFiles();
   }, 180000);
-  addAccessTask();
   console.log(`Server is running on port ${PORT}`);
 });
